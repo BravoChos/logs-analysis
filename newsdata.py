1e68500487a48161c734d2bc32f602d2552a902b
Song Cho <deeplearner507@gmail.com>
2019-02-16 18:00:53 +0900

    PEP8 Style applied, and fixed logics for /top_articles

--- newsdata.py
+++ newsdata.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
-# 
 # A buggy web service in need of a database.
 
 from flask import Flask, request, redirect, url_for
-
-from newsdatadb import get_most_popular_three_articles, get_most_popular_author, get_date_when_error_test_failed
+from newsdatadb import get_most_popular_three_articles, get_most_popular_author
+from newsdatadb import get_date_when_error_test_failed
 
 app = Flask(__name__)
 
@@ -58,7 +57,7 @@ HTML_WRAP = '''\
   </body>
 </html>
 '''
-   
+
 # # HTML template for an individual comment
 POST = '''\
   <p>"%s" -- %s views</p>
@@ -72,33 +71,46 @@ POST3 = '''\
   <p>%s -- %s errors</p>
 '''
 
+
 @app.route('/', methods=['GET'])
 def main():
-  '''Main page of the forum.'''
-  html = HTML_WRAP % ("","","")
-  return html
+    '''Main page of the forum.'''
+    html = HTML_WRAP % ("", "", "")
+    return html
+
 
 @app.route('/article/top_three_articles', methods=['GET'])
 def find_top_three_articles():
-  '''desplays top three articles'''
-  posts = "".join(POST % (path[9:], sum) for path, sum in get_most_popular_three_articles())
-  html = HTML_WRAP % (posts,"","")
-  return html
+    '''desplays top three articles'''
+    posts = "".join(
+        POST % (title, sum) for title, sum in
+        get_most_popular_three_articles()
+    )
+    html = HTML_WRAP % (posts, "", "")
+    return html
+
 
 @app.route('/article/best_authors', methods=['GET'])
 def find_best_authors():
-  '''displays the most popular authors in order'''
-  posts = "".join(POST2 % (author, views) for author, views in get_most_popular_author())
-  html = HTML_WRAP % ("",posts,"")
-  return html  
+    '''displays the most popular authors in order'''
+    posts = "".join(
+        POST2 % (author, views) for author, views
+        in get_most_popular_author()
+    )
+    html = HTML_WRAP % ("", posts, "")
+    return html
+
 
 @app.route('/article/percentage_error', methods=['GET'])
 def find_errors():
-  '''displays list of aritcle with percentage errors more than 1.0%'''
-  posts = "".join(POST3 % (date, format(percentage_error,'.2f')+'%') for date, percentage_error in get_date_when_error_test_failed())
-  html = HTML_WRAP % ("","",posts)
-  return html  
+    '''displays list of aritcle with percentage errors more than 1.0%'''
+    posts = "".join(
+        POST3 % (date, str(percentage_error) + '%') for date, percentage_error
+        in get_date_when_error_test_failed()
+    )
+    html = HTML_WRAP % ("", "", posts)
+    return html
+
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=8000)
-
+    app.run(host='0.0.0.0', port=8000)
--- newsdatadb.py
+++ newsdatadb.py
@@ -1,47 +1,93 @@
 # "Database code" for the DB News.
 
-import psycopg2, bleach
+import psycopg2
+import bleach
 
 DBNAME = "news"
 
+
 def get_most_popular_three_articles():
-  """Return most popluar three articles from the 'database', most recent first."""
-  db = psycopg2.connect(database=DBNAME)
-  c = db.cursor()
-  c.execute("select path, count(*) as sum from log WHERE log.path != '/' GROUP BY path ORDER BY sum desc limit 3")
-  result = c.fetchall()
-  db.close()
-  return result
+    """Return most popluar three articles
+    from the 'database', most recent first."""
+    db = psycopg2.connect(database=DBNAME)
+    c = db.cursor()
+    c.execute("""
+      SELECT title, count(log.path) as sum
+      FROM articles
+      JOIN log ON log.path like concat('%',articles.slug,'%')
+      GROUP BY title
+      ORDER BY sum desc limit 3
+      """)
+    result = c.fetchall()
+    output = '  "%s" -- %s views\n'
+    top_articles = "".join(output % (title, str(sum)) for title, sum in result)
+    print('\n1. What are the most popular three articles of all time?')
+    print(top_articles)
+    db.close()
+    return result
+
 
 def get_most_popular_author():
-  """Return most popluar article author of all time from the 'database' """
-  db = psycopg2.connect(database=DBNAME)
-  c = db.cursor()
-  c.execute(
-    """
-    select authors.name as author, count(log.path) as views 
-    from articles, authors, log 
-    WHERE authors.id = articles.author and log.path LIKE concat('%', articles.slug,'%') 
-    GROUP BY authors.name ORDER BY views desc
-    """
-  ) 
-  result = c.fetchall()
-  db.close()
-  return result
+    """Return most popluar article author of all time from the 'database' """
+    db = psycopg2.connect(database=DBNAME)
+    c = db.cursor()
+    c.execute(
+      """
+      SELECT authors.name AS author, count(log.path) AS views
+      FROM articles, authors, log
+      WHERE authors.id = articles.author
+        AND log.path = concat('/article/', articles.slug)
+      GROUP BY authors.name
+      ORDER BY views desc
+      """
+    )
+    result = c.fetchall()
+    output = '  "%s" -- %s views\n'
+    top_authors = "".join(output % (author, views) for author, views in result)
+    print('\n2. Who are the most popular article authors of all time?')
+    print(top_authors)
+    db.close()
+    return result
+
 
 def get_date_when_error_test_failed():
-  """Return days where more than 1% of requests lead to errors from the 'database' """
-  db = psycopg2.connect(database=DBNAME)
-  c = db.cursor()
-  c.execute(
-    """
-    select t.date, t.percentage_error 
-    from (select status_error.date as date, cast( (sum(status_error.errors))/(sum(status_error.errors)+sum(status_ok.ok))*100 as numeric) as percentage_error
-      from (select date(time) as date, count(status) as errors from log where status LIKE '404%' GROUP BY date) as status_error 
-    JOIN (select date(time) as date, count(status) as ok  from log where status LIKE '200%' GROUP BY date) as status_ok 
-    ON status_error.date = status_ok.date    
-    group by status_error.date ) as t WHERE t.percentage_error > 1.0
+    """Return days where more than 1% of requests lead to errors
+    from the 'database' """
+    db = psycopg2.connect(database=DBNAME)
+    c = db.cursor()
+    c.execute("""
+        SELECT
+            to_char(errorTest.date, 'Mon DD, YYYY'),
+            ROUND(errorTest.percentage_error, 2)
+        FROM (
+            SELECT status_error.date AS date,
+                CAST((SUM(status_error.errors))/(SUM(status_total.total)) * 100
+                AS numeric) AS percentage_error
+            FROM (SELECT
+                      date(time) AS date,
+                      count(status) AS errors
+                  FROM log
+                  WHERE status
+                  LIKE '404%'
+                  GROUP BY date) AS status_error
+            JOIN (SELECT
+                      date(time) AS date,
+                      count(status) AS total
+                  FROM log
+                  GROUP BY date) AS status_total
+            ON status_error.date = status_total.date
+            GROUP BY status_error.date
+        ) AS errorTest
+        WHERE errorTest.percentage_error > 1.0
     """)
-  result = c.fetchall()
-  db.close()
-  return result
+    result = c.fetchall()
+    output = '  %s -- %s'
+    error_test = "".join(
+        output % (str(date), str(error)) for date, error in result
+    )
+    print('\n3. On which days did more than 1 percentage of requests'
+        + ' lead to errors?'
+    )
+    print(error_test + ' %' + 'errors\n')
+    db.close()
+    return result
